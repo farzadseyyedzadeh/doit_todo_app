703dc83519bd0c5873e2bf505fcbdbf883d7ae2a
farzad seyyedzadeh <85734789+farzadseyyedzadeh@users.noreply.github.com>
2024-03-03 18:30:17 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class DoItApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
+      debugShowCheckedModeBanner: false,
       home: HomeScreen(),
     );
   }
